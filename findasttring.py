cbf0e24a3e0ff103bc0c68f65b3f0c09fac6b3e3
ram <puniaram0@gmail.com>
2020-09-24 16:09:28 +0530

    done

---
+++ findasttring.py
@@ -0,0 +1,14 @@
+def count_substring(string, sub_string):
+    
+    counts=0
+    for i in range(len(string)):
+        if string[i:i+len(sub_string)]==sub_string:
+            counts+=1
+    return counts
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
